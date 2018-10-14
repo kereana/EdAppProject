b723d995ef7a172db7b37cb32438557292490e44
kereana <kereana2008@hotmail.com>
2018-10-15 12:39:52 +1300

    finished adding photos to all the folders
    still need to add the images onto the different dictionary scenes
    add game objects to the createword scene

---
+++ Educational Project/Assets/Scripts/WordMaker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordMaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
