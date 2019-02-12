4c2941037378fb976ab944dfea4020afa41c19bf
kereana <kereana2008@hotmail.com>
2019-02-13 12:32:45 +1300

    Change scenes to objects
    because of the number of scenes, just trying to change some of the scenes to objects which should make program smaller

---
+++ Educational Project/Assets/Scripts/AScene2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AScene2 : MonoBehaviour {
+
+    public void Home()
+    {   //loading home scene once the home button is selected
+        SceneManager.LoadScene("Buttons");
+    
+    }
+
+}
